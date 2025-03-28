8fd8fc6f754a221720ea0214ea86dc3b89a577a2
Bastian2312 <bastian.adiputras@gmail.com>
2025-03-28 17:50:29 +0700

    init

---
+++ src/main/java/id/ac/ui/cs/advprog/berating/BeRatingApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.berating;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeRatingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeRatingApplication.class, args);
+    }
+
+}
