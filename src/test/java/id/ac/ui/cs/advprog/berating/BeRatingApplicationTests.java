ceaa6f92198dc63f681a53b8eaee3672aaa10639
StevenSetiawan <stevensetiawan1802@gmail.com>
2025-04-10 18:51:26 +0700

    feat: finish ci-cd

--- src/test/java/id/ac/ui/cs/advprog/berating/BeRatingApplicationTests.java
+++ src/test/java/id/ac/ui/cs/advprog/berating/BeRatingApplicationTests.java
@@ -1,13 +1,20 @@
 package id.ac.ui.cs.advprog.berating;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class BeRatingApplicationTests {
 
+    @Autowired
+    private ApplicationContext applicationContext;
+
     @Test
     void contextLoads() {
+        // Verify that the Spring application context loads successfully
+        assertThat(applicationContext).isNotNull();
     }
-
 }
