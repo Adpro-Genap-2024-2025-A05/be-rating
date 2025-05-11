3a3e6d7a876bb48f878084fc7aa709964d8321da
Aryan Primasatya <aryanstya238@gmail.com>
2025-05-11 23:56:03 +0700

    [GREEN] Implement the review controller

---
+++ src/main/java/id/ac/ui/cs/advprog/berating/controller/ReviewController.java
@@ -0,0 +1,101 @@
+package id.ac.ui.cs.advprog.berating.controller;
+
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import id.ac.ui.cs.advprog.berating.dto.BaseResponseDTO;
+import id.ac.ui.cs.advprog.berating.dto.ReviewRequest;
+import id.ac.ui.cs.advprog.berating.enums.ReviewStatus;
+import id.ac.ui.cs.advprog.berating.interfaces.ReviewService;
+import id.ac.ui.cs.advprog.berating.model.Review;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/reviews")
+@RequiredArgsConstructor
+@CrossOrigin(origins = "*")
+public class ReviewController {
+    private final ReviewService reviewService;
+
+    @GetMapping(value = "/{doctorId}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<BaseResponseDTO<List<Review>>> getDoctorReviews(@PathVariable("doctorId") UUID doctorId) {
+        BaseResponseDTO<List<Review>> baseResponseDTO = new BaseResponseDTO<>();
+        List<Review> reviews = reviewService.getDoctorReviews(doctorId);
+
+        baseResponseDTO.setStatus(HttpStatus.OK.value());
+        baseResponseDTO.setData(reviews);
+        baseResponseDTO.setMessage("Success to retrieve doctor reviews.");
+        baseResponseDTO.setTimestamp(new Date());
+
+        return ResponseEntity.ok(baseResponseDTO);
+    }
+
+    @PostMapping(value = "/create/{consultationId}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> createReview(
+            @PathVariable("consultationId") UUID consultationId,
+            @Valid @RequestBody ReviewRequest reviewRequest) {
+
+        try {
+            if (reviewRequest.getRating() == null) {
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Rating is required");
+            }
+
+            if (reviewRequest.getRating() < 1 || reviewRequest.getRating() > 5) {
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Rating must be between 1 and 5");
+            }
+
+            Review review = reviewService.createReview(consultationId, reviewRequest);
+            BaseResponseDTO<Review> baseResponseDTO = new BaseResponseDTO<>();
+            baseResponseDTO.setStatus(HttpStatus.CREATED.value());
+            baseResponseDTO.setData(review);
+            baseResponseDTO.setMessage("Success to retrieve doctor reviews.");
+            baseResponseDTO.setTimestamp(new Date());
+
+            return ResponseEntity.status(HttpStatus.CREATED).body(baseResponseDTO);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
+    }
+
+    @PatchMapping(value = "/{reviewId}/status", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> updateReviewStatus(
+            @PathVariable("reviewId") UUID reviewId,
+            @RequestParam("status") ReviewStatus status) {
+
+        try {
+            if (status == null) {
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Invalid status");
+            }
+
+            try {
+                Review updatedReview = reviewService.updateReviewStatus(reviewId, status);
+                BaseResponseDTO<Review> baseResponseDTO = new BaseResponseDTO<>();
+                baseResponseDTO.setStatus(HttpStatus.OK.value());
+                baseResponseDTO.setData(updatedReview);
+                baseResponseDTO.setMessage("Success to retrieve doctor reviews.");
+                baseResponseDTO.setTimestamp(new Date());
+
+                return ResponseEntity.ok(baseResponseDTO);
+            } catch (IllegalArgumentException e) {
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+            }
+        } catch (RuntimeException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
+        }
+    }
+}
