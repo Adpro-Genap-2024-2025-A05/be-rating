a5cd7dc6392caa759e4169be7e269bfee2819629
StevenSetiawan <stevensetiawan1802@gmail.com>
2025-05-23 19:27:07 +0700

    Fix problem

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,36 +28,34 @@ val seleniumJavaVersion = "4.14.1"
 val seleniumJupiterVersion = "5.0.1"
 val webdrivermanagerVersion = "5.6.3"
 val junitJupiterVersion = "5.9.1"
+val jjwtVersion = "0.11.5"
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("org.springframework.boot:spring-boot-starter-validation") 
-    
-    implementation("jakarta.servlet:jakarta.servlet-api")
-    implementation("jakarta.validation:jakarta.validation-api")
-    implementation("jakarta.persistence:jakarta.persistence-api")
-    
-    implementation("io.jsonwebtoken:jjwt-api:0.11.5")
-    implementation("io.jsonwebtoken:jjwt-impl:0.11.5")
-    implementation("io.jsonwebtoken:jjwt-jackson:0.11.5")
-    
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    runtimeOnly("org.postgresql:postgresql")
     compileOnly("org.projectlombok:lombok")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
     annotationProcessor("org.projectlombok:lombok")
-    
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testImplementation ("org.junit.jupiter:junit-jupiter:5.10.2")
+    testImplementation ("org.mockito:mockito-junit-jupiter:5.10.0")
+    testImplementation ("org.mockito:mockito-core:5.10.0")
     testImplementation("org.seleniumhq.selenium:selenium-java:$seleniumJavaVersion")
     testImplementation("io.github.bonigarcia:selenium-jupiter:$seleniumJupiterVersion")
     testImplementation("io.github.bonigarcia:webdrivermanager:$webdrivermanagerVersion")
     testImplementation("org.junit.jupiter:junit-jupiter:$junitJupiterVersion")
-    testImplementation("com.h2database:h2")
-    
     implementation("org.postgresql:postgresql:42.6.0")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("me.paulschwarz:spring-dotenv:3.0.0")
+    testImplementation("com.h2database:h2")
+    implementation("io.jsonwebtoken:jjwt-api:${jjwtVersion}")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:${jjwtVersion}")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:${jjwtVersion}")
 }
 
 tasks.register<Test>("unitTest") {
--- src/test/java/id/ac/ui/cs/advprog/berating/BeRatingApplicationTests.java
+++ src/test/java/id/ac/ui/cs/advprog/berating/BeRatingApplicationTests.java
@@ -4,9 +4,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
-
-import id.ac.ui.cs.advprog.berating.BeRatingApplication;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
@@ -24,5 +21,6 @@ class BeRatingApplicationTests {
     @Test
     void testMainMethod() {
         BeRatingApplication.main(new String[]{});
+        assertThat(true).isTrue();
     }
 }
